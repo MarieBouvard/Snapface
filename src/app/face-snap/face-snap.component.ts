cc8f6e2ab441a8c587656b1467be84a8784c6496
mariebouvard <mariebouvard@outlook.com>
2022-05-24 14:20:30 +0200

    Single page Angular application

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapService } from '../services/face-snaps.service';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+  buttonText!: string;
+
+  constructor(private faceSnapsService: FaceSnapService,
+              private route: Router) {}
+
+  ngOnInit(){
+    this.buttonText = "Oh Snap!";
+  }
+
+
+  onAddSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+        this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+        this.buttonText = 'Oops, unSnap!';
+    } else {
+        this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+        this.buttonText = 'Oh Snap!';
+    }
+  }
+
+  onViewFaceSnap(){
+      this.route.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
+  }
+
+}
